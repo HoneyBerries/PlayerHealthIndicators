14e8dddff835fb14a5e21491d20ab1221b7aa7f1
HoneyBerries <henry.rainbowfish@gmail.com>
2026-05-04 13:26:39 -0700

    Initial Commit

---
+++ src/client/java/net/honeyberries/client/PlayerHealthIndicatorsClient.java
@@ -0,0 +1,10 @@
+package net.honeyberries.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayerHealthIndicatorsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
