b19a792a0bc9a667421f05d8f484a59495a67b91
Henry Ng <henry.rainbowfish@gmail.com>
2026-05-04 14:28:07 -0700

    Delete net directory

--- net/minecraft/client/renderer/blockentity/state/HangingSignRenderState.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.client.renderer.blockentity.state;
-
-import net.minecraft.world.level.block.HangingSignBlock.Attachment;
-
-public class HangingSignRenderState extends SignRenderState {
-	public Attachment attachmentType = Attachment.CEILING;
-}
--- net/minecraft/client/renderer/entity/state/MushroomCowRenderState.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.client.renderer.entity.state;
-
-import net.minecraft.client.renderer.block.BlockModelRenderState;
-import net.minecraft.world.entity.animal.cow.MushroomCow.Variant;
-
-public class MushroomCowRenderState extends LivingEntityRenderState {
-	public Variant variant = Variant.RED;
-	public final BlockModelRenderState mushroomModel = new BlockModelRenderState();
-}
